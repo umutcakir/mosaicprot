5901ba329559007e74485c0a8226ac3524eb93ba
umutcakir <hpumut@gmail.com>
2025-06-02 18:58:06 +0200

    Update command.sh

--- example_files/inputs/command.sh
+++ example_files/inputs/command.sh
@@ -1,1 +1,4 @@
-shiftscan --nucleotide_file transcriptome.fasta --peptide_file frameshifted_peptides.fasta
+mosaicprot detect_ORFs --transcriptome_file transcriptome.fasta
+mosaicprot separate_ORFs --ORFeome_file transcriptome_min_30aa_ORFs.fasta --reference_proteome_file refProt.fasta
+mosaicprot simulate_chimeric_proteins --candidate_altProt_list candidate_list.txt --transcriptome_file transcriptome.fasta
+
