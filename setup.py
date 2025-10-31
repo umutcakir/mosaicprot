3205b6cb282420ff9e31d582dfeed04b6c7aaabc
Ali Yurtseven <58915125+aliyurtsevenn@users.noreply.github.com>
2025-10-31 22:02:37 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mosaicprot",
-    version="0.1.6",
+    version="0.1.12",
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
